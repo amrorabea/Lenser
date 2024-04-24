802ce99723320783e90fa3bd547a25523c8b7f37
MRMomo11111 <125286452+MRMomo11111@users.noreply.github.com>
2024-04-24 13:33:49 +0200

    Update object_detection.dart
    فتح الكميرا

--- source_code/lib/pages/object_detection.dart
+++ source_code/lib/pages/object_detection.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-class ObjectDetection extends StatelessWidget {
+late List<CameraDescription> cameras;
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:  ObjectDetection(),
+    );
+  }
+}
+class ObjectDetection extends StatefulWidget {
   const ObjectDetection({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  State<ObjectDetection> createState() => _ObjectDetectionState();
 }
+late CameraController _controller;
+class _ObjectDetectionState extends State<ObjectDetection> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller=CameraController(cameras[0], ResolutionPreset.max);
+     _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {
+
+      });
+    }).catchError((Object e){
+      if(e is CameraException){
+        switch (e.code){
+          case 'cameraAccessdenid':
+          print("access was denied");
+          break;
+          default:
+          print(e.description);
+          break;
+        }
+      }
+        
+      
+
+    });
+    
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+    
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+   
+      body:  Stack(children: [
+        Container(
+          height: double.infinity,
+          child: CameraPreview(_controller),
+        )
+      ]),
+    );
+  }
